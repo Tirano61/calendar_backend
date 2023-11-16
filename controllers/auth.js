e3b86adc04ca5dd3248c3c4117165dd0777baf97
Dario <dramirez@balanzashook.com.ar>
2023-11-16 15:57:26 -0300

    validacion usuarios

---
+++ controllers/auth.js
@@ -0,0 +1,50 @@
+const { response } = require('express');
+const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario');
+
+
+
+const crearUsuario = async (req, res = response) => {
+  // const { name, email, password } = req.body;
+  try {
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+      
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'No se pudo crear usuario'
+    })
+  }
+}
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
+  
+  res.json({
+    ok: true,
+    msg: 'login',
+    email,
+    password
+
+  });
+} 
+const revalidarToken = (req, res = response) => {
+
+  res.json({
+    ok: true,
+    msg: 'renew'
+  });
+}
+
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 const express = require('express');
+const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
 
@@ -8,8 +9,14 @@ require('dotenv').config();
 // Crear el servidor de express
 const app = express();
 
+// Base de datos
+dbConnection();
+
 //Directorio publico
 app.use(express.static('public'))
+
+// Lectura y parseo del body
+app.use(express.json());
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
